ec8be3bf05d3b4de9e1b42c29d5624dc1361ca2a
Samuellatham <141748415+Samuellatham@users.noreply.github.com>
2024-09-09 07:42:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,18 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>woooooooo</h1>
-    <?php
-    echo "hooray";
+
+<form mehtod="GET">
+<input type="text" name="person">
+<button>SUBMIT</button>
+</form>
+
     
+    <?php
+    echo "hooray "; 
+    print "tim walz is a terrorist ";
+    $name = $_GET['person'] ;
+    echo $name."2024";
     ?>
 </body>
 </html>
