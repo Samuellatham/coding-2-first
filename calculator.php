9e09fca328d1626151b1fa0a0667ba3ec354fc6f
Samuellatham <141748415+Samuellatham@users.noreply.github.com>
2024-09-11 07:45:29 -0500

    Create calculator.php

---
+++ calculator.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>calculator</title>
+</head>
+<body>
+     <form> 
+        <input type="text" name="num1" placeholder="Number 1">
+        <input type="text" name="num2" placeholder="Number 2">
+        <select name="operator" >
+            <option>none</option>
+            <option>add</option>
+            <option>subtract</option>
+            <option>multiply</option>
+            <option>divide</option>
+        </select>
+        <br>
+        <button name="submit" value="submit" type="submit">Calculate</button>
+     </form>
+     <p>The answer is:</p>
+     <?php
+        if(isset($_GET['submit'])){
+            $result1 = $_GET['num1'];
+            $result2 = $_GET['num2'];
+            $operator = $_GET['operator'];
+            switch($operator){
+                case "none":
+                    echo "You need to select an operator!";
+                    break;
+                case "add":
+                    echo $result1 + $result2;
+                    break;
+                case "subtract":
+                    echo $result1 - $result2;
+                    break;
+                case "multiply":
+                    echo $result1 * $result2;
+                     break;
+                case "divide":
+                    echo $result1 / $result2;
+                    break;
+            }
+        }
+     ?>
+</body>
+</html>
