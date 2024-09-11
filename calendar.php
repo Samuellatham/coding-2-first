ebc372aca2d4b7c41e4a2e7a685bfe86d14bb10c
Samuellatham <141748415+Samuellatham@users.noreply.github.com>
2024-09-11 07:59:54 -0500

    df

---
+++ calendar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <?php
+$dayofweek = date("w");
+
+switch ($dayofweek){
+    case 1:
+        echo "<p>It is monday</p>";
+        break;
+    case 2:
+        echo "<p>It is tuesday</p>";
+         break;
+    case 3:
+         echo "<p>It is wednesday</p>";
+        break;
+    case 4:
+        echo "<p>It is Thursday</p>";
+        break;
+    case 5:
+        echo "<p>It is Friday</p>";
+        break;
+    case 6:
+        echo "<p>It is Saturday</p>";
+        break;
+    case 0:
+        echo "<p>It is Sunday</p>";
+        break;
+}
+    ?>
+</body>
+</html>
